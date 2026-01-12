9cec8146a24255810f4fe13fd2df668b5fff0660
Smilie Pop <arisatominato48@gmail.com>
2026-01-12 23:21:50 +0800

    refactor: moved user seed to another file

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // 1. Admin user
+        $admin = User::create([
+            "name" => "System Administrator",
+            "email" => "admin@example.com",
+            "password" => bcrypt("password"), // might change in production, but as of now let's leave it at that :>
+        ]);
+        $admin->assignRole("admin");
+
+        // 2. Support Manager user
+        $manager = User::create([
+            "name" => "Support Manager",
+            "email" => "manager@example.com",
+            "password" => bcrypt("password"), // might change in production, but as of now let's leave it at that :>
+        ]);
+        $manager->assignRole("support-manager");
+
+        // 3. Support Agent user
+        $agent = User::create([
+            "name" => "Support Agent",
+            "email" => "agent@example.com",
+            "password" => bcrypt("password"), // might change in production, but as of now let's leave it at that :>
+        ]);
+        $agent->assignRole("agent");
+
+        // 4. Regular User
+        $user = User::create([
+            "name" => "Test User",
+            "email" => "user@example.com",
+            "password" => bcrypt("password"), // might change in production, but as of now let's leave it at that :>
+        ]);
+        $user->assignRole("user");
+    }
+}
