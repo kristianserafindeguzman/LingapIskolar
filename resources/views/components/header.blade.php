1c3cc64d97d9244139ffcef78a8439c8df077210
Smilie Pop <arisatominato48@gmail.com>
2026-01-13 18:34:58 +0800

    fix(design): added stylish design

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,15 @@
-<header class="box-border flex flex-row items-center justify-between px-8 py-4 border-b-2 border-red-800">
+<header
+    class="box-border flex flex-row items-center justify-between border-b-2 border-red-800 px-8 py-4"
+>
     <div class="left text-3xl">
         <a href="{{ route("root") }}">
             <img src="/img/home-logo.jpg" alt="" class="w-64" />
         </a>
     </div>
-    <div class="right flex gap-4">
+    <div class="right flex flex-1 gap-4 justify-end">
         @auth
-            <x-button :href="route('ticket-create')">New Ticket</x-button>
+            @section("side")
+            @show
         @endauth
 
         @guest
--- resources/views/components/select-input.blade.php
+++ resources/views/components/select-input.blade.php
@@ -1,8 +1,10 @@
-<div class="relative">
+<div class="group relative w-full">
     @if ($icon)
-        <i
-            class="bi {{ $icon }} absolute top-1/2 left-4 -translate-y-1/2 text-4xl text-neutral-800"
-        ></i>
+        <div
+            class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-5 text-zinc-400 transition-colors group-focus-within:text-red-800"
+        >
+            <i class="bi {{ $icon }} text-xl"></i>
+        </div>
     @endif
 
     <select
@@ -11,10 +13,15 @@
                 "class" => $getStyleOfInput(),
                 "id" => $id,
                 "name" => $id,
-                "value" => $value,
             ])
         }}
     >
         {{ $slot }}
     </select>
+
+    <div
+        class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4 text-zinc-400 group-focus-within:text-red-800"
+    >
+        <i class="bi bi-chevron-down text-sm"></i>
+    </div>
 </div>
